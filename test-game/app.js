f3859507a64179ff8c3e5ddbe514ab6379ea166a
Shane M. Ross <sross@blu20.com>
2017-01-05 03:08:06 -0600

    Figured out keybinding
    Press Control or Command + X ;)

--- test-game/app.js
+++ test-game/app.js
@@ -1,17 +1,27 @@
 const electron = require('electron')
-const { app } = electron
-const { BrowserWindow } = electron
+const { app, BrowserWindow, globalShortcut } = electron
+const Mousetrap = require('../node_modules/mousetrap/mousetrap')
 
 app.on('ready', () => {
-    let browser = new BrowserWindow({
-        width: 1280,
-        height: 720,
-        backgroundColor: '#000000'
+
+    const exit = globalShortcut.register('CommandOrControl+Q', () => {
+        app.quit()
     })
-    browser.setMenu(null)
+
+    let browser = new BrowserWindow({
+            width: 1280,
+            height: 720,
+            backgroundColor: '#000000'
+        })
+        //browser.setMenu(null)
     browser.loadURL(`file://${__dirname}/index.html`)
 
     browser.on('closed', () => {
         browser = null
     })
+})
+
+app.on('will-quit', () => {
+    globalShortcut.unregister('CommandOrControl+Q')
+    globalShortcut.unregisterAll()
 })
